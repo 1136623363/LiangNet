6d50dda60cf4bfea0d32d4320e2b8fe58ccedfef
1136623363 <1136623363@qq.com>
2024-02-29 15:08:09 +0800

    init

--- train_new.py
+++ train_new.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0249390e1c09f7afdd63b7735a9d3271d6aa55c52419ce20519c41a37e999b19
-size 27280
+oid sha256:c42739dda938d527e8edd1a3a876a113f53fa10ebe7e3d964adf38dd8f21f0b0
+size 27293
